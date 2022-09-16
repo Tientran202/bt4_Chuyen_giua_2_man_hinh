5131f01a545620085c1098bcca2f5160027df716
Tientran202 <tranvuongngoctien@gmail.com>
2022-09-16 16:27:32 +0700

    bt 4 chuyen giua 2 man hinh

---
+++ app/src/main/java/com/example/bt4_di_chuyen2man_hinh/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.bt4_di_chuyen2man_hinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,manhinh2.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/bt4_di_chuyen2man_hinh/manhinh2.java
@@ -0,0 +1,27 @@
+package com.example.bt4_di_chuyen2man_hinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class manhinh2 extends AppCompatActivity {
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinh2);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(manhinh2.this,MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
